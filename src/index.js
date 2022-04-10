113fb1e9db19fa2cc277f77c9ccafcfec4aa44ee
Leissan Khan <leissan.khan@gmail.com>
2022-04-10 13:48:27 -0400

    write pseudo code

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/*
+Write out pseudo code in steps:
+
+Create a DOMContentLoaded event to get all anime 
+
+Create a fetch request to json provided 
+add each anime object from json to the DOM as li with links inside
+make each link live (clickable) by adding event listener
+
+add event listener to a like button which creates a pop-up with a reminder to watch it
+
+add event listener to all anime button which loads all anime 
+
+
+*/
